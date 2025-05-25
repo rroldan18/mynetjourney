559bb075e688f3d2af302ad0acd38a2eaa1a3516
rroldan18 <rodenroldan18@gmail.com>
2025-05-25 16:51:59 +0800

    Add files via upload

---
+++ telephony AI.ps1
@@ -0,0 +1,37 @@
+﻿config t
+dial-peer voice 69 voip
+ service rivanaa out-bound
+ destination-pattern 5169
+ session target ipv4:10.51.100.8
+ incoming called-number 5169
+ dtmf-relay h245-alphanumeric
+ codec g711ulaw
+ no vad
+!
+telephony-service
+ moh "flash:/en_bacd_music_on_hold.au"
+!
+application
+ service rivanaa flash:app-b-acd-aa-3.0.0.2.tcl
+  paramspace english index 1        
+  param number-of-hunt-grps 2
+  param dial-by-extension-option 8
+  param handoff-string rivanaa
+  param welcome-prompt flash:en_bacd_welcome.au
+  paramspace english language en
+  param call-retry-timer 15
+  param service-name rivanqueue
+  paramspace english location flash:
+  param second-greeting-time 60
+  param max-time-vm-retry 2
+  param voice-mail 1234
+  param max-time-call-retry 700
+  param aa-pilot 5169
+ service rivanqueue flash:app-b-acd-3.0.0.2.tcl
+  param queue-len 15
+  param aa-hunt1 5100
+  param aa-hunt2 5177
+  param aa-hunt3 5101
+  param aa-hunt4 5133
+  param queue-manager-debugs 1
+  param number-of-hunt-grps 4
---
+++ voice register.ps1
@@ -0,0 +1,53 @@
+﻿conf t
+ voice service voip
+  allow-connections h323 to sip    
+  allow-connections sip to h323
+  allow-connections sip to sip
+  supplementary-service h450.12
+ sip
+   bind control source-interface fa0/0
+   bind media source-interface fa0/0
+   registrar server expires max 600 min 60
+!
+ voice register global
+  mode cme
+  source-address 10.51.100.8 port 5060
+  max-dn 12
+  max-pool 12
+  authenticate register
+  create profile sync
+ voice register dn 1
+   number 5128
+   allow watch
+   name 5128
+ voice register dn 2
+   number 5127
+   allow watch
+   name 5127
+!
+  voice register pool 1
+    id mac 6c48.3f3b.94c2
+    number 1 dn 1
+    dtmf-relay sip-notify
+    username 5128 password 5128
+    codec g711ulaw
+!
+  voice register pool 2
+    id mac 76e5.5268.cba5
+    number 1 dn 2
+    dtmf-relay sip-notify
+    username 5127 password 5127
+    codec g711ulaw
+!
+*******call other countries!!!********
+config t
+!
+dial-peer voice _ voip
+destination-pattern k..
+session protocol sipv2
+session target sip-server
+codec g711ulaw
+!
+          
+sip-ua
+sip-server ipv4:192.168.k.1
